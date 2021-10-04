a3e7bd20838e318f2e145ff0aee7d971b86c1aa9
tohirbek009 <odilovtohirbek009@gmail.com>
2021-10-04 11:38:55 +0500

    modal was fixed

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ function getModalStyled() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     position: 'absolute',
-    width: 400,
+    width: 250,
     backgroundColor: theme.palette.background.paper,
     border: "2px solid #000",
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3)
+    padding: theme.spacing(1, 2, 1)
   },
 }));
 
